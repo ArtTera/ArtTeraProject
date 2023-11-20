a20d332eea594143a8e316a503c7220a1ba1195b
Your Name <you@example.com>
2023-11-20 18:44:44 +0100

    .

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,17 @@
 import { Link } from "react-router-dom";
+
+const containerStyle = {
+  width: "100vw",
+  height: "100vh",
+  backgroundImage: `url(src/assets/image-from-rawpixel-id-6233550-jpeg.jpg)`,
+  backgroundPosition: "center",
+  backgroundSize: "cover",
+  backgroundRepeat: "no-repeat",
+};
+
 const HomePage = () => {
   return (
-    <div className="home">
+    <div style={containerStyle} className="home">
       <h1 className="home-title">ArtTera</h1>
       <Link to="/painting">
         <button>VIEW ME!</button>
--- src/pages/Painting.jsx
+++ src/pages/Painting.jsx
@@ -1,38 +1,45 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+
 import axios from "axios";
+
 function Painting() {
-  const [pic,setPic]= useState([]);
-  const getPic =() =>{
+  const [pic, setPic] = useState([]);
+
+  const getPic = () => {
     axios
-    .get("https://appi.adaptable.app/pic")
-  
-  .then ((Response)=>{
-    setPic(Response.data);
-  console.log(Response);
-  })
-  .catch((error) => {
-    console.log(error);})
-  };
-    useEffect(() => {
-      getPic();
-  }, []);
-  const addToCart = (picId) => {
-    console.log({picId});
+      .get("https://appi.adaptable.app/pic")
+
+      .then((Response) => {
+        setPic(Response.data);
+        console.log(Response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
+  useEffect(() => {
+    getPic();
+  }, []);
+  const addToCart = (picId) => {
+    console.log({ picId });
+  };
 
   return (
     <div className="pic">
       <h1>Paintings</h1>
+
       {pic.map((item) => (
         <div key={item.id}>
           <img src={item.img} alt={item.title} />
           <h1>{item.title}</h1>
           <h2>{item.price}</h2>
           <p>{item.description}</p>
+
           <Link to={`/cart/${item.id}`}>
-          <button onClick={() => addToCart(item.id)}>ADD TO CART</button></Link>
+            <button onClick={() => addToCart(item.id)}>ADD TO CART</button>
+          </Link>
         </div>
       ))}
     </div>
