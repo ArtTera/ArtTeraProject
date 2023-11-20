4eeeed998016e871305a24b55400437cbef25f2f
Menatalla Gomaa <minafathi98@gmail.com>
2023-11-20 15:41:08 +0100

    new Bg

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,20 @@
 import Painting from "./Painting";
 
+const containerStyle = {
+  width: "100vw",
+  height: "100vh",
+  backgroundImage: `url(src/assets/image-from-rawpixel-id-6233550-jpeg.jpg)`,
+  backgroundPosition: "center",
+  backgroundSize: "cover",
+  backgroundRepeat: "no-repeat",
+};
+
 const HomePage = () => {
   return (
-    <div className="home">
+    <div style={containerStyle} className="home">
       <h1 className="home-title">ArtTera</h1>
       <button onClick={Painting}> View Me! </button>
     </div>
   );
-}
+};
 export default HomePage;
