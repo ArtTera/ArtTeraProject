63fedcbf79770dc4f9cf38b629774a522aab28fe
Your Name <you@example.com>
2023-11-21 13:47:10 +0100

    adding Add AddPaintingPage

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,21 @@ import HomePage from "./pages/Homepage";
 import Cart from "./pages/Cart";
 import Painting from "./pages/Painting";
 import PaymentMethod from "./pages/PaymentMethods";
-
+import About from "./components/about";
+import AddPaintingPage from "./pages/addPaintingPage";
 
 function App() {
   return (
-    <div  
-    >
-      <Navbar /><Routes>
-          <Route path="/" exact element={<HomePage />} />
-          <Route path="/cart/:id" element={<Cart/>} />
-          <Route path="/Painting" element={<Painting />} />
-          <Route path="/PaymentMethod" element={<PaymentMethod />} />
-        </Routes>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<HomePage />} />
+        <Route path="/cart/:id" element={<Cart />} />
+        <Route path="/Painting" element={<Painting />} />
+        <Route path="/PaymentMethod" element={<PaymentMethod />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/addPaintingPage" element={<AddPaintingPage />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,18 +4,11 @@ import "./Navbar.css";
 function Navbar() {
   return (
     <nav className="Navbar">
-<<<<<<< HEAD
       <NavLink to="/">Home</NavLink> |{" "}
       <NavLink to="src/components/about.jsx">About</NavLink>|{" "}
       <NavLink to="src/components/contact.jsx">contact</NavLink>|{" "}
-      <NavLink to="src/components/Help!.jsx">HELP?</NavLink>|{" "}
+      <NavLink to="src/components/Help!.jsx">HELP?</NavLink>
 
-=======
-      <NavLink to="/" exact>
-        Home
-      </NavLink>{" "}
-      | <NavLink to="about">About us</NavLink>
->>>>>>> de52f4f65363dc0e219e74534c542ce187a27d92
     </nav>
   );
 }
