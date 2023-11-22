0c510cc24a183ee76432059a1c8cae65ab0b39be
Menatalla Gomaa <minafathi98@gmail.com>
2023-11-22 17:05:59 +0100

    hello test

--- src/App.jsx
+++ src/App.jsx
@@ -9,22 +9,22 @@ import AddPaintingPage from "./pages/addPaintingPage";
 import EditItem from "./pages/EditItem";
 import Footer from "./components/Footer";
 
-
-
 function App() {
   return (
-      <><Navbar /><Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/cart/:id" element={<Cart />} />
-      <Route path="/Painting" element={<Painting />} />
-      <Route path="/PaymentMethod" element={<PaymentMethod />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/addPaintingPage" element={<AddPaintingPage />} />
-      <Route path="/EditItem/:picId" element={<EditItem />} />
-      <Route path="/Footer" element={<Footer />} />
-
-    </Routes></>
-    
+    <>
+      <Navbar />
+      <Routes>
+        <h1>Hello world</h1>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/cart/:id" element={<Cart />} />
+        <Route path="/Painting" element={<Painting />} />
+        <Route path="/PaymentMethod" element={<PaymentMethod />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/addPaintingPage" element={<AddPaintingPage />} />
+        <Route path="/EditItem/:picId" element={<EditItem />} />
+        <Route path="/Footer" element={<Footer />} />
+      </Routes>
+    </>
   );
 }
 
