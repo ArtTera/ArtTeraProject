d15fcd518a9d0b9ef2bfa17fb295984e97a4270a
Menatalla Gomaa <minafathi98@gmail.com>
2024-01-31 17:34:34 +0100

    Linking the Signup/Login pages to the Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,9 @@ function Navbar() {
   return (
     <nav className="Navbar">
       <NavLink to="/">Home</NavLink> | <NavLink to="/about">About</NavLink> |{" "}
-      <NavLink to="/addPaintingPage">Create</NavLink>
+      <NavLink to="/addPaintingPage">Create</NavLink> |{" "}
+      <NavLink to="/SignupPage">Signup</NavLink> |{" "}
+      <NavLink to="/LoginPage">Login</NavLink>
     </nav>
   );
 }
